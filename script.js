6f5b33a7b43a32b518e41ff81af84b132af9b201
Boldis Aron <aron.boldis@katonaj-mkszig.hu>
2023-12-01 09:38:03 +0100

    -

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
-let user_selection;
+let user_selection = null;
+let cpuRoll = null;
 
 document.getElementById("rock").addEventListener("click", function(){
     user_selection = 1;
+    cpuRoll = cpuSelection();
 });
 document.getElementById("paper").addEventListener("click", function(){
     user_selection = 2;
+    cpuRoll = cpuSelection();
 });
 document.getElementById("scissors").addEventListener("click", function(){
     user_selection = 3;
-    console.log(cpuSelection);
+    cpuRoll = cpuSelection();
 });
 
 function cpuSelection(){
-    let cpuRoll = Math.floor(Math.random() * 4) + 1;
+    let cpuRoll = Math.floor(Math.random() * 3) + 1;
     return cpuRoll;
 }
